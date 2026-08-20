cceec5e89d9b0eb799bb4770fd39af31ec504bfd
SantiOAdesign <santiagoosorio2006@gmail.com>
2026-08-20 14:04:29 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ from gtts import gTTS
 from PIL import Image
 import base64
 
-st.title("Conversión de Texto a Audio")
-image = Image.open('gato_raton.png')
+st.title("Conversión de Texto a Audio por Santiago Osorio")
+image = Image.open('IAAudio.jpg')
 st.image(image, width=350)
 with st.sidebar:
     st.subheader("Esrcibe y/o selecciona texto para ser escuchado.")
