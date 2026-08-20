4953054d2a4e01cfb24919ade5362b200335a811
SantiOAdesign <santiagoosorio2006@gmail.com>
2026-08-20 14:06:56 -0500

    Update character in fable from mouse to AI

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ except:
     pass
 
 st.subheader("Una pequeña Fábula.")
-st.write('¡Ay! -dijo el ratón-. El mundo se hace cada día más pequeño. Al principio era tan grande que le tenía miedo. '  
+st.write('¡Ay! -dijo la IA-. El mundo se hace cada día más pequeño. Al principio era tan grande que le tenía miedo. '  
          ' Corría y corría y por cierto que me alegraba ver esos muros, a diestra y siniestra, en la distancia. ' 
          ' Pero esas paredes se estrechan tan rápido que me encuentro en el último cuarto y ahí en el rincón está '  
          ' la trampa sobre la cual debo pasar. Todo lo que debes hacer es cambiar de rumbo dijo el gato...y se lo comió. ' 
